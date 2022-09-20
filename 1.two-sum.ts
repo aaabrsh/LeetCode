c44a775204e57320ba79841ca339d60b26e2d18f
Abrham Alemayehu <aaabrham421@gmail.com>
2022-09-20 13:39:23 +0300

    two-sum

---
+++ 1.two-sum.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    let output: number[] = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                output[0] = i;
+                output[1] = j;
+                return output;
+            }
+        }
+    }
+};
+// @lc code=end
+
