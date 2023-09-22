f285aba75d694b38f549ebd0a135ec755cc0fb15
abraham <aaabrham421@gmail.com>
2023-09-22 15:14:11 +0300

    reverse-linked-list

---
+++ 206.reverse-linked-list.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=206 lang=typescript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  const values: number[] = [];
+  let cur = head;
+
+  while (cur) {
+    values.push(cur.val);
+    cur = cur.next;
+  }
+
+  let res = new ListNode();
+  const res_head = res;
+  for (let i = values.length - 1; i >= 0; i--) {
+    res.next = { val: values[i], next: null };
+    res = res.next;
+  }
+
+  return res_head.next;
+}
+// @lc code=end
