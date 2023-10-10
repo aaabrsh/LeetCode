62e66be01ce3e066f17c9753154cb111c05c0b49
abraham <aaabrham421@gmail.com>
2023-10-10 11:46:30 +0300

    power-of-four

---
+++ 342.power-of-four.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=342 lang=typescript
+ *
+ * [342] Power of Four
+ */
+
+// @lc code=start
+function isPowerOfFour(n: number): boolean {
+  if (n === 1) {
+    return true;
+  } else if (n % 4 !== 0 || n === 0) {
+    return false;
+  }
+
+  return isPowerOfFour(n / 4);
+}
+// @lc code=end
