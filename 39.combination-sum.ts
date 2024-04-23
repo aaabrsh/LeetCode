49a9ad9f228f3fc741f5cb444b13460c35c3cebc
aaabrham <aaabrham421@gmail.com>
2024-04-23 21:06:33 +0300

    combination-sum: Update

--- 39.combination-sum.ts
+++ 39.combination-sum.ts
@@ -8,15 +8,6 @@
 function combinationSum(candidates: number[], target: number): number[][] {
   candidates = candidates.sort((a, b) => a - b);
 
-  let all_candidates: number[] = [];
-  candidates.forEach((num, i) => {
-    // run the repeatNumber function for unique numbers
-    if (num !== candidates[i - 1]) {
-      // repeat the numbers that need to be repeated
-      all_candidates.push(...repeatNumber(num, target));
-    }
-  });
-
   const combinations: number[][] = [];
 
   //  build the combinations and for each combination, check if the sum is equal to target
@@ -30,35 +21,31 @@ function combinationSum(candidates: number[], target: number): number[][] {
       return;
     }
 
-    for (let i = start; i < all_candidates.length; i++) {
-      if (i > start && all_candidates[i] === all_candidates[i - 1]) continue; // to avoid duplicate checking skip the numbers that are the same as the previous number
+    for (let i = start; i < candidates.length; i++) {
+      if (i > start && candidates[i] === candidates[i - 1]) continue; // to avoid duplicate checking skip the numbers that are the same as the previous number
 
-      const temp = [...combination, all_candidates[i]].sort((a, b) => a - b);
+      const temp = [...combination, candidates[i]].sort((a, b) => a - b);
 
-      let newSum = sum + all_candidates[i];
+      let newSum = sum + candidates[i];
 
       if (newSum === target) {
         combinations.push(temp);
         return;
       } else if (newSum < target) {
-        buildCombinations(iteration - 1, i + 1, temp, newSum);
+        /**
+         * we pass `i` instead of `i + 1` and `iteration` instead of `iteration - 1` to the next recursion,
+         * becasue we want to check if the repitition of the current number can reach the target
+         */
+        buildCombinations(iteration, i, temp, newSum);
       } else {
         return;
       }
     }
   };
 
-  buildCombinations(all_candidates.length);
+  buildCombinations(candidates.length);
 
   return combinations;
-}
-
-// repeat numbers as long as their sum is less than the target
-function repeatNumber(num: number, target: number): number[] {
-  const quotient = Math.floor(target / num);
-  const res: number[] = new Array(quotient).fill(num);
-
-  return res;
 }
 
 // @lc code=end
