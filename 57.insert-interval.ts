5f24eca4a2b33e1653a4238bf84ce9f16a09ae9b
aaabrham <aaabrham421@gmail.com>
2024-11-13 21:06:45 +0300

    insert-interval

--- 57.insert-interval.ts
+++ 57.insert-interval.ts
@@ -6,10 +6,7 @@
 
 // @lc code=start
 function insert(intervals: number[][], newInterval: number[]): number[][] {
-  if (!intervals.length) return [newInterval];
-
   const res: number[][] = [];
-  let insert: number[] = [...newInterval];
 
   let i = 0;
   while (i < intervals.length) {
@@ -24,13 +21,13 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
       break;
     } else {
       // if interval overlaps with newInterval, take the min and max of the intervals to merge them
-      insert[0] = Math.min(intervals[i][0], insert[0]);
-      insert[1] = Math.max(intervals[i][1], insert[1]);
+      newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+      newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
     }
     i++;
   }
 
-  res.push(insert);
+  res.push(newInterval);
   res.push(...intervals.slice(i)); // add remaining intervals
 
   return res;
